50ebd2c415c0b5d575110e35fc5e77074d93c7a4
nguy0510 <44421112+nguy0510@users.noreply.github.com>
2019-05-29 21:21:53 -0400

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import * as Discord from "discord.js";
+import * as ConfigFile from "./config";
+import { IBotCommands } from "./api";
+
+const discBot: Discord.Client = new Discord.Client();
+
+let commands : IBotCommands[] = [];
+
+loadCommands(`${__dirname}/commands`)
+
+discBot.on("ready", () => {
+    //lets us know the bot is online
+    console.log("Ready to go");
+})
+
+discBot.on("message", msg => {
+    //ignore the message if it was sent by the bot
+    if(msg.author.bot){return;}
+
+    //ignore messages that don't start with the prefix
+    if(!msg.content.startsWith(ConfigFile.config.prefix)){return;}
+
+    msg.channel.send(`${msg.author.username} just used a command!`);
+})
+
+async function commandHandler(msg: Discord.Message){
+
+    //Split the string into the command and all of the args
+    let command = msg.content.split(" ")[0].replace(ConfigFile.config.prefix, "");
+    let args = msg.content.split(" ").slice(1);
+
+    for(const commandsClass of commands){
+        //attempt to execute code 
+        try{
+            //Iterates through the command array and checks if the command class is the correct one
+            if(!commandsClass.isThisCommand(command)){
+                //Go to the next iteration of the loop if this isn't the correct command class
+                continue;
+            }
+            //Pause execution while we run command's code
+            await commandsClass.runCommand(args, msg,discBot);
+        }
+        catch (exception){
+            //if there is an error, logs the exception
+            console.log(exception);
+        }
+    }
+}
+
+function loadCommands(commandsPath: string){
+    //Exit if there are no commands
+    if(!ConfigFile.config || (ConfigFile.config.commands as string[]).length ===0){ return; }
+
+    //Loop through command array 
+    for(const commandName of ConfigFile.config.commands as string[]){
+
+        const commnadsClass = require(`${commandsPath}/${commandName}`).default;
+
+        const command = new commnadsClass() as IBotCommands;
+
+        commands.push(command);
+    }
+}
+
+discBot.login(ConfigFile.config.token);
